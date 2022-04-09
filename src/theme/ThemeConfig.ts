42f686e8ac81741490f1348790e37a08d0061476
Arslan Sajid <arslansajid951@gmail.com>
2022-04-09 23:45:59 +0200

    Added theme to app

---
+++ src/theme/ThemeConfig.ts
@@ -0,0 +1,31 @@
+import { colors } from "./Colors";
+
+export const theme = {
+  white: colors.white,
+  primaryColor: colors.primaryColor,
+  secondaryColor: colors.secondaryColor,
+  textColor: colors.textColor,
+  backgroundColor: colors.backgroundColor,
+  headingColor: colors.headingColor,
+  dividerColor: colors.dividerColor,
+  labelColor: colors.labelColor,
+
+  //   flexboxgrid: {
+  //     // Defaults
+  //     gridSize: 12, // columns
+  //     gutterWidth: 1, // rem
+  //     outerMargin: 2, // rem
+  //     mediaQuery: "only screen",
+  //     container: {
+  //       sm: 46, // rem
+  //       md: 61, // rem
+  //       lg: 76, // rem
+  //     },
+  //     breakpoints: {
+  //       xs: 0, // em
+  //       sm: 48, // em
+  //       md: 64, // em
+  //       lg: 75, // em
+  //     },
+  //   },
+};
