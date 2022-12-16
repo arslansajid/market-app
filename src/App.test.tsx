eac8eb9723927f6d31349576c01c82969d060ce0
Arslan Sajid <arslansajid951@gmail.com>
2022-12-16 15:54:11 +0100

    Updated unit case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { storeInitialState } from "./static/store";
+
+describe("With React Testing Library", () => {
+  const mockStore = configureStore();
+  let store;
+
+  it('Shows "Application!"', () => {
+    store = mockStore(storeInitialState);
+    const { getByAltText, getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    expect(getByAltText("logo")).not.toBeNull();
+    expect(getByText("Products")).not.toBeNull();
+  });
 });
